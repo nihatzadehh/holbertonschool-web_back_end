f1a53a3a049a8c8588d46d56bd5249041426fec9
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-30 18:24:42 +0400

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
-"""Script that provides some stats about Nginx logs stored in MongoDB"""
-
+""" Pretty logs in python with Mongo """
 from pymongo import MongoClient
+
+
+def count(mg_col, method):
+    """ Writing an simple count function for reducing the repetition """
+
+    res = mg_col.count_documents({"method": method})
+    return res
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    nginx_collection = db.nginx
+    ng_collection = client.logs.nginx
 
-    # Total logs
-    total_logs = nginx_collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    # Methods stats
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{ng_collection.count_documents({})} logs")
     print("Methods:")
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    # Status check (GET /status)
-    status_check = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+    for i in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(f"	method {i}: {count(ng_collection, i)}")
+    print("{} status check".format(ng_collection.count_documents({"path": "/status"})))
